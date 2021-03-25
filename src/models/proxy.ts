054b1c419f6501e295b244412ce4070e6725e8af
man90 <man90@tuta.io>
2021-03-25 12:37:38 +0100

    Add query factories

--- src/models/proxy.ts
+++ src/models/proxy.ts
@@ -9,6 +9,16 @@
 import { IProxy } from "./../types/proxy"
 import pool from "./index"
 
+const queries = {
+	selectProxies: () => {
+		return "select address, port, added_at as \"addedAt\", checked_at as \"checkedAt\" from proxies"
+	},
+
+	insertProxy: (address: string, port: number) => {
+		return `insert into proxies (address, port, added_at) values ('${address}', ${port}, to_timestamp(${Date.now()} / 1000.0))`
+	}
+}
+
 export class Proxy implements IProxy {
 	address?: string
 	port?: number
@@ -30,7 +40,7 @@ export class Proxy implements IProxy {
 
 	static find() {
 		return new Promise<IProxy[]>((resolve, reject) => {
-			pool.query("select address, port, added_at as \"addedAt\", checked_at as \"checkedAt\" from proxies", (error, results) => {
+			pool.query(queries.selectProxies(), (error, results) => {
 				error ? reject(error) : resolve(results.rows.map(row => new Proxy(row)))
 			})
 		})
@@ -38,9 +48,14 @@ export class Proxy implements IProxy {
 
 	save() {
 		return new Promise<IProxy>((resolve, reject) => {
-			pool.query(`insert into proxies (address, port, added_at) values ('${this.address}', ${this.port}, to_timestamp(${Date.now()} / 1000.0))`, (error, results) => {
-				error ? reject(error) : resolve((this as unknown) as IProxy)
-			})
+			if (this.address === undefined || this.port === undefined) {
+				reject()
+			} else {
+				pool.query(queries.insertProxy(this.address, this.port), (error, results) => {
+					error ? reject(error) : resolve((this as unknown) as IProxy)
+				})
+			}
+			
 		})
 	}
 }
