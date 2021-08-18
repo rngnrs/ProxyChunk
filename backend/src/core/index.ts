11d2a171ec003efa61cf606baff42609bc57a87e
man90 <man90@tuta.io>
2021-08-18 09:33:42 +0200

    Communicate the error better if can't launch proxyshiva

--- backend/src/core/index.ts
+++ backend/src/core/index.ts
@@ -16,7 +16,20 @@ if (process.env.TIMEOUT !== undefined) {
 	shivaParams.push(`-timeout=${process.env.TIMEOUT}`)
 }
 
-const shiva = spawn("ProxyShiva", shivaParams)
+const shiva = spawn("proxyshiva", shivaParams)
+
+shiva.on("error", (err) => {
+	if (err.message.includes("ENOENT")) {
+		console.log(
+			"Looks like you don't have proxyshiva installed in your system\n" +
+			"Refer to the README file for installation instructions"
+		)
+	} else {
+		console.log("Unknown critical error occured:", err.message)
+	}
+
+	process.exit(1)
+})
 
 shiva.stdin.setDefaultEncoding("utf-8")
 
