b66629d5ffba5032214dba484f54aac0a3790a40
man90 <man90@tuta.io>
2021-08-23 11:39:35 +0200

    Add navigation links between the home page and the control panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
+	Link,
 } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
@@ -9,21 +10,24 @@ import AdminPage from './views/Admin'
 import HomePage from './views/Home'
 import './App.scss'
 
-
 export default function App() {
-	return (
-		<>
-			<Router>
-				<Switch>
-					<Route path="/admin">
-						<AdminPage />
-					</Route>
-					<Route path="/">
-						<HomePage />
-					</Route>
-				</Switch>
-			</Router>
-			<Toaster />
-		</>
-	)
+	return <>
+		<Router>
+			<header>
+				<Link to="/">ProxyChunk</Link>
+			</header>
+			<Switch>
+				<Route path="/admin">
+					<AdminPage />
+				</Route>
+				<Route path="/">
+					<HomePage />
+				</Route>
+			</Switch>
+			<footer>
+				<Link to="/admin">Control panel</Link>
+			</footer>
+		</Router>
+		<Toaster />
+	</>
 }
