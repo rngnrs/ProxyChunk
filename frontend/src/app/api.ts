5bf7ee489353a95b1c1f34c5557090fea978b0ef
man90 <man90@tuta.io>
2022-04-05 09:50:30 +0200

    Remove unnecessary error rethrowing

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -4,26 +4,18 @@ import { IProxy } from '../types'
 
 export const api = {
 	getProxies: async (page: number = 0, goodOnly: boolean = true): Promise<{page: number, totalPages: number, proxies: IProxy[]}> => {
-		try {
-			return axios.get(
-				`${process.env.REACT_APP_API_ENDPOINT}/proxies?page=${page}&goodOnly=${goodOnly}`,
-				{ withCredentials: true }
-			).then(response => response.data)
-		} catch (error) {
-			throw new Error(error)
-		}
+		return axios.get(
+			`${process.env.REACT_APP_API_ENDPOINT}/proxies?page=${page}&goodOnly=${goodOnly}`,
+			{ withCredentials: true }
+		).then(response => response.data)
 	},
 
 	postProxies: async ({schemes, addresses, ports}: {schemes: Set<string>, addresses: [string, string], ports: [number, number]}): Promise<void> => {
-		try {
-			await axios.post(
-				`${process.env.REACT_APP_API_ENDPOINT}/proxies`,
-				{ schemes: Array.from(schemes), addresses, ports },
-				{ withCredentials: true }
-			)
-		} catch (error) {
-			throw new Error(error)
-		}
+		return await axios.post(
+			`${process.env.REACT_APP_API_ENDPOINT}/proxies`,
+			{ schemes: Array.from(schemes), addresses, ports },
+			{ withCredentials: true }
+		)
 	},
 
 	login: async ({accessCode}: {accessCode?: string} = {}): Promise<{user: string}> => {
