ace1921ed03ba2ed1d2f72b25419bd6873901a6a
man90 <man90@tuta.io>
2022-04-07 09:43:13 +0200

    Rewrite time difference calculation to component

--- frontend/src/features/proxyList/Line.tsx
+++ frontend/src/features/proxyList/Line.tsx
@@ -1,43 +1,44 @@
 import { IProxy } from '../../types'
 import { num2Word } from '../../utils'
 
-function timeDiff(date: Date) {
-	return +new Date() - +new Date(date)
-}
-
-function timeDiffToText(ms: number) {
-	if (ms < 4e4){
-		return 'Just now'
-	}
-
-	let minutes = Math.round(ms / 6e4)
-	if (minutes < 50){
-		return `${minutes} ${num2Word(minutes, ['minute', 'minutes'])} ago`
-	}
-
-	let hours = Math.round(ms / 3.6e6)
-	if (hours < 20){
-		return `${hours} ${num2Word(hours, ['hour', 'hours'])} ago`
-	}
-
-	let days = Math.round(ms / 8.64e7)
-	if (1 === days){
-		return "yesterday"
-	}
-
-	return `${days} ${num2Word(days, ['day', 'days'])} ago`
-}
-
 function SpeedDiv({ speed }: { speed: number }) {
 	let formattedSpeed: string
 
 	if (speed < 0) {
-		formattedSpeed = "Bad"
+		formattedSpeed = 'Bad'
 	} else {
-		formattedSpeed = speed.toFixed(2) + "ms"
+		formattedSpeed = speed.toFixed(2) + 'ms'
 	}
 
 	return <div>{ formattedSpeed }</div>
+}
+
+function TimeDiffDiv({ updatedAt }: { updatedAt: Date }) {
+	const delta = +new Date() - +new Date(updatedAt)
+	const formatted = (() => {
+		if (delta < 4e4){
+			return 'Just now'
+		}
+
+		let minutes = Math.round(delta / 6e4)
+		if (minutes < 50){
+			return `${minutes} ${num2Word(minutes, ['minute', 'minutes'])} ago`
+		}
+
+		let hours = Math.round(delta / 3.6e6)
+		if (hours < 20){
+			return `${hours} ${num2Word(hours, ['hour', 'hours'])} ago`
+		}
+
+		let days = Math.round(delta / 8.64e7)
+		if (1 === days){
+			return 'yesterday'
+		}
+
+		return `${days} ${num2Word(days, ['day', 'days'])} ago`
+	})()
+
+	return <div>{ formatted }</div>
 }
 
 export default function ProxyList({ proxy }: { proxy: IProxy }) {
@@ -46,7 +47,7 @@ export default function ProxyList({ proxy }: { proxy: IProxy }) {
 			<div className="proxy-list-line">
 				<div>{ proxy.scheme }://{ proxy.address }:{ proxy.port }</div>
 				<SpeedDiv speed={ proxy.speed } />
-				<div>{ timeDiffToText(timeDiff(proxy.updatedAt)) }</div>
+				<TimeDiffDiv updatedAt={ proxy.updatedAt }/>
 			</div>
 		)
 	} else {
@@ -56,7 +57,7 @@ export default function ProxyList({ proxy }: { proxy: IProxy }) {
 				<div>{ proxy.address }</div>
 				<div>{ proxy.port }</div>
 				<SpeedDiv speed={ proxy.speed } />
-				<div>{ timeDiffToText(timeDiff(proxy.updatedAt)) }</div>
+				<TimeDiffDiv updatedAt={ proxy.updatedAt }/>
 			</div>
 		)
 	}
